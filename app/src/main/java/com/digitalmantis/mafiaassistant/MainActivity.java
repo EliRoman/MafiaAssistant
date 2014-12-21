049c757b74e543e92b9fd63d3ecbb6384d9b3d7b
DetX <detx@outlook.com>
2014-12-20 19:15:03 -0500

    general cleaning

--- app/src/main/java/com/digitalmantis/mafiaassistant/MainActivity.java
+++ app/src/main/java/com/digitalmantis/mafiaassistant/MainActivity.java
@@ -22,26 +22,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity implements OnClickListener {
 
-	private Button btnStart;
-	private EditText txtPlayers;
-	private TextView lblInstructions, lblCredit;
-	private int mode = 0, playerCount;
-	private String[] roles;
-	private int curPlayerIndex = 0;
-	private String[] players;
-	
+    private Button btnStart;
+    private EditText txtPlayers;
+    private TextView lblInstructions, lblCredit;
+    private int phase = 0, playerCount, curPlayerIndex = 0;
+    private String[] roles, players;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         // Inflate the UI
-        btnStart = (Button)findViewById(R.id.btnStart);
-        txtPlayers = (EditText)findViewById(R.id.txtPlayers);
-        lblCredit = (TextView)findViewById(R.id.lblCredit);
-        lblInstructions = (TextView)findViewById(R.id.lblInstructions);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        txtPlayers = (EditText) findViewById(R.id.txtPlayers);
+        lblCredit = (TextView) findViewById(R.id.lblCredit);
+        lblInstructions = (TextView) findViewById(R.id.lblInstructions);
         btnStart.setOnClickListener(this);
     }
 
@@ -66,122 +64,119 @@ public class MainActivity extends Activity implements OnClickListener{
     }
 
 
-	@Override
-	public void onClick(View arg0) {
-		if(arg0.equals(btnStart)){
-			
-			// Handle the player count and the names
-			if(mode == 0){
+    @Override
+    public void onClick(View arg0) {
+        // Handle the player count and the names
+        if (phase == 0) {
 
-				lblInstructions.setTextColor(Color.BLACK);
-				lblInstructions.setText("");
-				String input = txtPlayers.getText().toString();
-				
-				// Get the number of players and instantiate the array with that many spots
-				if(input.length() != 0 && Integer.parseInt(input) >= 6){
-					playerCount = Integer.parseInt(txtPlayers.getText().toString());
-					players = new String[playerCount];
-					assignRoles();
-					
-					// Tell the players to enter their names
-					lblCredit.setText("");
-					btnStart.setText("Next");
-					// Allow text for name input
-					txtPlayers.setInputType(1);
-					mode = 1;
-					txtPlayers.setText("");
-					lblInstructions.setText("Please enter player 1's name.");
-					txtPlayers.setHint("Player name");
-				}else{
-					lblInstructions.setTextColor(Color.RED);
-					lblInstructions.setText("You must have at least 6 players!");
-				}
+            lblInstructions.setTextColor(Color.BLACK);
+            lblInstructions.setText("");
+            String input = txtPlayers.getText().toString();
 
-			}else if(mode == 1){
-				// Assign a name and role to each player
-				if(curPlayerIndex < playerCount){
-					lblInstructions.setText("Please enter player " + (curPlayerIndex + 2) + "'s name");
-					String playerName;
-					
-					if(!txtPlayers.getText().toString().equals("")){
-						playerName = txtPlayers.getText().toString();
-					}else{
-						playerName = "Player " + (curPlayerIndex + 1);
-					}
-					
-					players[curPlayerIndex] = roles[curPlayerIndex].toString() + "##" + playerName;
-					
+            // Get the number of players and instantiate the array with that many spots
+            if (input.length() != 0 && Integer.parseInt(input) >= 6) {
+                playerCount = Integer.parseInt(txtPlayers.getText().toString());
+                players = new String[playerCount];
+                assignRoles();
 
-					// Display the current player's role
-					AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-					alertDialog.setButton("Close",new DialogInterface.OnClickListener() {
-				        public void onClick(DialogInterface dialog, int id) {
-				            dialog.cancel();
-				        }
-				    });
-					alertDialog.setTitle("Player Created");
-					alertDialog.setMessage("Congratulations, " + playerName + "!\nYou are " + roles[curPlayerIndex] + ".");
-					alertDialog.show();
-					
-					// Clear the text box and move on to the next player
-					txtPlayers.setText("");
-					curPlayerIndex++;
-					
-					// Finish and move on
-					if(curPlayerIndex == playerCount){
-						mode = 2;
-						lblInstructions.setText("Press Next to Begin");
-						txtPlayers.setVisibility(View.INVISIBLE);
-					}
-				}
-			}else if(mode == 2){
-				// Create a new intent focused on the game activity and pass in the player count
-				Intent gameActivity = new Intent();
-				gameActivity.setClassName("com.digitalmantis.mafiaassistant", "com.digitalmantis.mafiaassistant.VoteScreen");
-												
-				gameActivity.putExtra("players.array.key", players);
+                // Tell the players to enter their names
+                lblCredit.setText("");
+                btnStart.setText("Next");
+                // Allow text for name input
+                txtPlayers.setInputType(1);
+                phase = 1;
+                txtPlayers.setText("");
+                lblInstructions.setText("Please enter player 1's name.");
+                txtPlayers.setHint("Player name");
+            } else {
+                lblInstructions.setTextColor(Color.RED);
+                lblInstructions.setText("You must have at least 6 players!");
+            }
 
-				startActivity(gameActivity);
-			}
-			
-		}
-	}
-	
-	String[] assignRoles(){
-		
-		// Fill an array with the appropriate roles in a standard fashion
-		roles = new String[playerCount];
-		
-		int numMafia = playerCount / 3;
-		
-		for(int i = 0; i <= numMafia; i++){
-			roles[i] = "Mafia";
-		}
-		
-		for(int i = numMafia; i < playerCount; i++){
-			roles[i] = "Innocent";
-		}
-		
-		roles[playerCount - 2] = "Detective";
-		roles[playerCount - 1] = "Doctor";
-		
-		// Shuffle the roles and return the array
-		return shuffle(roles);
-	}
-	
-	// Shuffle an array randomly
-	String[] shuffle(String[] array){
-		int currentIndex = array.length, randomIndex ;
-		String temporaryValue;
-		
-		while (0 != currentIndex) {
-			randomIndex = (int) Math.floor(Math.random() * currentIndex);
-			currentIndex -= 1;
-		    temporaryValue = array[currentIndex];
-		    array[currentIndex] = array[randomIndex];
-		    array[randomIndex] = temporaryValue;
-		}
-		
+        } else if (phase == 1) {
+            // Assign a name and role to each player
+            if (curPlayerIndex < playerCount) {
+                lblInstructions.setText("Please enter player " + (curPlayerIndex + 2) + "'s name");
+                String playerName;
+
+                if (!txtPlayers.getText().toString().equals("")) {
+                    playerName = txtPlayers.getText().toString();
+                } else {
+                    playerName = "Player " + (curPlayerIndex + 1);
+                }
+
+                players[curPlayerIndex] = roles[curPlayerIndex].toString() + "##" + playerName;
+
+
+                // Display the current player's role
+                AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+                alertDialog.setButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+                alertDialog.setTitle("Player Created");
+                alertDialog.setMessage("Congratulations, " + playerName + "!\nYou are " + roles[curPlayerIndex] + ".");
+                alertDialog.show();
+
+                // Clear the text box and move on to the next player
+                txtPlayers.setText("");
+                curPlayerIndex++;
+
+                // Finish and move on
+                if (curPlayerIndex == playerCount) {
+                    phase = 2;
+                    lblInstructions.setText("Press Next to Begin");
+                    txtPlayers.setVisibility(View.INVISIBLE);
+                }
+            }
+        } else if (phase == 2) {
+            // Create a new intent focused on the game activity and pass in the player count
+            Intent gameActivity = new Intent();
+            gameActivity.setClassName("com.digitalmantis.mafiaassistant", "com.digitalmantis.mafiaassistant.VoteScreen");
+
+            gameActivity.putExtra("players.array.key", players);
+
+            startActivity(gameActivity);
+        }
+
+    }
+
+    String[] assignRoles() {
+
+        // Fill an array with the appropriate roles in a standard fashion
+        roles = new String[playerCount];
+
+        int numMafia = playerCount / 3;
+
+        for (int i = 0; i <= numMafia; i++) {
+            roles[i] = "Mafia";
+        }
+
+        for (int i = numMafia; i < playerCount; i++) {
+            roles[i] = "Innocent";
+        }
+
+        roles[playerCount - 2] = "Detective";
+        roles[playerCount - 1] = "Doctor";
+
+        // Shuffle the roles and return the array
+        return shuffle(roles);
+    }
+
+    // Shuffle an array randomly
+    String[] shuffle(String[] array) {
+        int currentIndex = array.length, randomIndex;
+        String temporaryValue;
+
+        while (0 != currentIndex) {
+            randomIndex = (int) Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
         return array;
-	}
+    }
 }
--- app/src/main/java/com/digitalmantis/mafiaassistant/VoteScreen.java
+++ app/src/main/java/com/digitalmantis/mafiaassistant/VoteScreen.java
@@ -7,245 +7,247 @@
 
 package com.digitalmantis.mafiaassistant;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class VoteScreen extends Activity implements OnClickListener{
+import java.util.ArrayList;
 
-	private int curDay = 0, phaseIndex = 0;
-	private Player[] players;
-	private TextView lblCurDay, lblResults;
-	private Button btnNextPhase;
-	private boolean gameOver = false, innocentVictory = false, docIsDead = false, detectiveIsDead = false;;
-	private String choiceID;
-	private int choiceIndex, innocentCount = 0, mafiaCount = 0;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_vote_screen);
-		lblCurDay = (TextView)findViewById(R.id.lblCurDay);
-		lblResults = (TextView)findViewById(R.id.lblResults);
-		btnNextPhase = (Button)findViewById(R.id.btnNextPhase);
-		btnNextPhase.setOnClickListener(this);
-		
-		// Get the string array and instantiate the players array
-		String[] temp = getIntent().getStringArrayExtra("players.array.key");
-		players = new Player[temp.length];
-		
-		// Take the array of pseudo-players and turn them into real players
-		for(int i = 0; i < temp.length; i++){
-			String[] curPlayer = temp[i].split("\\#\\#");
-			players[i] = new Player(curPlayer[0], curPlayer[1]);
-            if(players[i].getRole().equals("Mafia")){
+public class VoteScreen extends Activity implements OnClickListener {
+
+
+    private Player[] players;
+    private TextView lblCurDay, lblResults;
+    private Button btnNextPhase;
+    private boolean gameOver = false, innocentVictory = false, docIsDead = false, detectiveIsDead = false;
+    private String choiceID;
+    private int curDay = 0, phaseIndex = 0, choiceIndex, innocentCount = 0, mafiaCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote_screen);
+        lblCurDay = (TextView) findViewById(R.id.lblCurDay);
+        lblResults = (TextView) findViewById(R.id.lblResults);
+        btnNextPhase = (Button) findViewById(R.id.btnNextPhase);
+        btnNextPhase.setOnClickListener(this);
+
+        // Get the string array and instantiate the players array
+        String[] temp = getIntent().getStringArrayExtra("players.array.key");
+        players = new Player[temp.length];
+
+        // Take the array of pseudo-players and turn them into real players
+        for (int i = 0; i < temp.length; i++) {
+            String[] curPlayer = temp[i].split("\\#\\#");
+            players[i] = new Player(curPlayer[0], curPlayer[1]);
+            if (players[i].getRole().equals("Mafia")) {
                 mafiaCount++;
-            }else{
+            } else {
                 innocentCount++;
             }
-		}
-		
-		lblCurDay.setText("Night: " + (curDay + 1));
-		
-	}
-	
-	public void doVote(final String voter){
-		ArrayList<String> dialogItems = new ArrayList<String>();
-		
-		// Build the menu appropriately
-		for(int i = 0; i < players.length; i++){
-			
-			// If the current player is not dead, then add them to the voting list
-			if(!players[i].getHealthStatus().equals("Dead")){
-				if(!players[i].getRole().equals(voter) || players[i].getRole().equals("Doctor") || players[i].getRole().equals("Innocent")){
-					dialogItems.add(players[i].getName());
-				}
-			}else{
-				continue;
-			}
-		}
+        }
 
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		
-		if(voter.equals("Detective")){
-			builder.setTitle("Who to investigate?");
-		}else if(voter.equals("Mafia")){
-			builder.setTitle("Who to Kill");
-		}else if(voter.equals("Doctor")){
-			builder.setTitle("Who to Save");
-		}else if(voter.equals("Innocent")){
-			builder.setTitle("Who to Exile");
-		}
-		
-			builder.setItems(dialogItems.toArray(new String[dialogItems.size()]), new DialogInterface.OnClickListener() {
-			    @Override
-			    public void onClick(DialogInterface dialog, int selection) {
-			    	ArrayList<String> dialogIDs = new ArrayList<String>();
-					
-					// Build the menu appropriately
-					for(int i = 0; i < players.length; i++){
-						
-						// If the current player is not dead, then add them to the voting list
-						if(!players[i].getHealthStatus().equals("Dead")){
-							if(!players[i].getRole().equals(voter) || players[i].getRole().equals("Doctor") || players[i].getRole().equals("Innocent")){
-								dialogIDs.add(players[i].getPlayerID());
-							}
-						}else{
-							continue;
-						}
+        lblCurDay.setText("Night: " + (curDay + 1));
 
-					}
-			    	
-			        choiceIndex = selection;
-			        choiceID = dialogIDs.get(choiceIndex);
-			        
-			        for(int i = 0; i < players.length; i++){
-						if(players[i].getPlayerID().equals(choiceID)){
-							calculateVote(voter, players[i]);
-							break;
-						}
-					}
-			    }
-			});
-			builder.show();
-		
-	}
+    }
+
+    public void doVote(final String voter) {
+        ArrayList<String> dialogItems = new ArrayList<String>();
+
+        // Build the menu appropriately
+        for (int i = 0; i < players.length; i++) {
+
+            // If the current player is not dead, then add them to the voting list
+            if (!players[i].getHealthStatus().equals("Dead")) {
+                if (!players[i].getRole().equals(voter) || players[i].getRole().equals("Doctor") || players[i].getRole().equals("Innocent")) {
+                    dialogItems.add(players[i].getName());
+                }
+            } else {
+                continue;
+            }
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        if (voter.equals("Detective")) {
+            builder.setTitle("Who to investigate?");
+        } else if (voter.equals("Mafia")) {
+            builder.setTitle("Who to Kill");
+        } else if (voter.equals("Doctor")) {
+            builder.setTitle("Who to Save");
+        } else if (voter.equals("Innocent")) {
+            builder.setTitle("Who to Exile");
+        }
+
+        builder.setItems(dialogItems.toArray(new String[dialogItems.size()]), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int selection) {
+                ArrayList<String> dialogIDs = new ArrayList<String>();
+
+                // Build the menu appropriately
+                for (int i = 0; i < players.length; i++) {
+
+                    // If the current player is not dead, then add them to the voting list
+                    if (!players[i].getHealthStatus().equals("Dead")) {
+                        if (!players[i].getRole().equals(voter) || players[i].getRole().equals("Doctor") || players[i].getRole().equals("Innocent")) {
+                            dialogIDs.add(players[i].getPlayerID());
+                        }
+                    } else {
+                        continue;
+                    }
+
+                }
+
+                choiceIndex = selection;
+                choiceID = dialogIDs.get(choiceIndex);
+
+                for (int i = 0; i < players.length; i++) {
+                    if (players[i].getPlayerID().equals(choiceID)) {
+                        calculateVote(voter, players[i]);
+                        break;
+                    }
+                }
+            }
+        });
+        builder.show();
+
+    }
 
     // Dialogs and basic actions to perform when voting
-	public void calculateVote(String voter, Player player){
-		if(voter.equals("Detective")){
-			// Display the investigated player's role
-			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-			alertDialog.setButton("Close",new DialogInterface.OnClickListener() {
-		        public void onClick(DialogInterface dialog, int id) {
-		            dialog.cancel();
-		        }
-		    });
-			alertDialog.setTitle("Player Investigated");
-			alertDialog.setMessage(player.getName() + " is " + player.getRole() + "!");
-			alertDialog.show();
-		}else if(voter.equals("Mafia")){
-			player.setHealthStatus(player.getHealthTypes()[2]);
-			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-			alertDialog.setButton("Close",new DialogInterface.OnClickListener() {
-		        public void onClick(DialogInterface dialog, int id) {
-		            dialog.cancel();
-		        }
-		    });
-			alertDialog.setTitle("Player Hit");
-			alertDialog.setMessage("You put a hit on " + player.getName() + "!");
-			alertDialog.show();
-		}else if(voter.equals("Doctor")){
-			// Save the player
-			player.setHealthStatus(player.getHealthTypes()[0]);
-			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-			alertDialog.setButton("Close",new DialogInterface.OnClickListener() {
-		        public void onClick(DialogInterface dialog, int id) {
-		            dialog.cancel();
-		        }
-		    });
-			alertDialog.setTitle("Player Saved");
-			alertDialog.setMessage(player.getName() + " is safe from the Mafia!");
-			alertDialog.show();
-		}else if(voter.equals("Innocent")){
-			// Exile the player
-			player.setHealthStatus(player.getHealthTypes()[1]);
-            if(player.getRole().equals("Mafia")){
+    public void calculateVote(String voter, Player player) {
+
+        // TODO: Find alternative to setButton()
+        if (voter.equals("Detective")) {
+            // Display the investigated player's role
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setButton("Close", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+            alertDialog.setTitle("Player Investigated");
+            alertDialog.setMessage(player.getName() + " is " + player.getRole() + "!");
+            alertDialog.show();
+        } else if (voter.equals("Mafia")) {
+            player.setHealthStatus(player.getHealthTypes()[2]);
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setButton("Close", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+            alertDialog.setTitle("Player Hit");
+            alertDialog.setMessage("You put a hit on " + player.getName() + "!");
+            alertDialog.show();
+        } else if (voter.equals("Doctor")) {
+            // Save the player
+            player.setHealthStatus(player.getHealthTypes()[0]);
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setButton("Close", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+            alertDialog.setTitle("Player Saved");
+            alertDialog.setMessage(player.getName() + " is safe from the Mafia!");
+            alertDialog.show();
+        } else if (voter.equals("Innocent")) {
+            // Exile the player
+            player.setHealthStatus(player.getHealthTypes()[1]);
+            if (player.getRole().equals("Mafia")) {
                 mafiaCount--;
-            }else{
+            } else {
                 innocentCount--;
             }
 
             //detect if the exiled player has a special role so that later we wont call them
-            if(player.getRole().equals("Detective")){
+            if (player.getRole().equals("Detective")) {
                 detectiveIsDead = true;
             }
-            if(player.getRole().equals("Doctor")){
+            if (player.getRole().equals("Doctor")) {
                 docIsDead = true;
             }
 
             checkVictory();
-			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-			alertDialog.setButton("Close",new DialogInterface.OnClickListener() {
-		        public void onClick(DialogInterface dialog, int id) {
-		            dialog.cancel();
-		        }
-		    });
-			alertDialog.setTitle("Player Exiled");
-			alertDialog.setMessage(player.getName() + " was " + player.getRole() + "!");
-			alertDialog.show();
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setButton("Close", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+            alertDialog.setTitle("Player Exiled");
+            alertDialog.setMessage(player.getName() + " was " + player.getRole() + "!");
+            alertDialog.show();
             phaseIndex = 0;
-		}
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.vote_screen, menu);
-		return true;
-	}
+        }
+    }
 
-	@Override
-	public void onClick(View arg0) {
-		if(!gameOver){
-            if(phaseIndex == 0){
-                if(!detectiveIsDead){//if detective is not dead display detective screen
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.vote_screen, menu);
+        return true;
+    }
+
+    @Override
+    public void onClick(View arg0) {
+        if (!gameOver) {
+            if (phaseIndex == 0) {
+                if (!detectiveIsDead) {//if detective is not dead display detective screen
                     doVote("Detective");
                 }
                 phaseIndex++;
-            }else if(phaseIndex == 1){
+            } else if (phaseIndex == 1) {
                 doVote("Mafia");
                 phaseIndex++;
-            }else if(phaseIndex == 2){
+            } else if (phaseIndex == 2) {
 
-                if(!docIsDead){//if doctor is not dead display doctor screen
+                if (!docIsDead) {//if doctor is not dead display doctor screen
                     doVote("Doctor");
                 }
                 phaseIndex++;
-            }else if(phaseIndex == 3){
-                showResults();			curDay++;
+            } else if (phaseIndex == 3) {
+                showResults();
+                curDay++;
                 lblCurDay.setText("Day: " + (curDay + 1));
                 phaseIndex++;
-            }else if(phaseIndex == 4){
+            } else if (phaseIndex == 4) {
                 doVote("Innocent");
 
                 phaseIndex = 0;
             }
-        }else{
-            if(innocentVictory){
+        } else {
+            if (innocentVictory) {
                 lblResults.setText("Daily Happenings:\n\nThe mafia have been driven out of Ponty Pandy and the innocents have won!");
-            }else{
+            } else {
                 lblResults.setText("Daily Happenings:\n\nThe innocents have been driven out of Ponty Pandy and the mafia have won!");
             }
         }
-	}
+    }
 
-    public void showResults(){
-        for(int i = 0; i < players.length; i++){
-            if(players[i].getHealthStatus().equals(players[i].getHealthTypes()[2])){
+    public void showResults() {
+        for (int i = 0; i < players.length; i++) {
+            if (players[i].getHealthStatus().equals(players[i].getHealthTypes()[2])) {
                 players[i].setHealthStatus(players[i].getHealthTypes()[1]);
 
                 innocentCount--;
 
                 //detect if the killed player has a special role so that later we wont call them
-                if(players[i].getRole().equals("Detective")){
+                if (players[i].getRole().equals("Detective")) {
                     detectiveIsDead = true;
                 }
-                if(players[i].getRole().equals("Doctor")){
+                if (players[i].getRole().equals("Doctor")) {
                     docIsDead = true;
                 }
 
                 // TODO: Add more flavor text for a more engaging experience
-                lblResults.setText("Daily Happenings:\n\n" + players[i].getName() + " has died in a horrific accident!\n" + players[i].getName() + " was " + players[i].getRole()+"!");
+                lblResults.setText("Daily Happenings:\n\n" + players[i].getName() + " has died in a horrific accident!\n" + players[i].getName() + " was " + players[i].getRole() + "!");
                 break;
             }
 
@@ -256,11 +258,11 @@ public class VoteScreen extends Activity implements OnClickListener{
 
     // If the mafia outnumber the innocent, then they win.
     // If there are no more mafia, then the innocents win
-    public void checkVictory(){
-        if(mafiaCount > innocentCount){
+    public void checkVictory() {
+        if (mafiaCount > innocentCount) {
             gameOver = true;
             innocentVictory = false;
-        }else if(mafiaCount == 0){
+        } else if (mafiaCount == 0) {
             gameOver = true;
             innocentVictory = true;
         }
